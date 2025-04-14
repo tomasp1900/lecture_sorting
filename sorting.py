d199aeafa2ef24981f21b59750a0ef3c649dbbfe
Tomas Prchlik <tomasp1900@gmail.com>
2025-04-14 14:08:56 +0200

    message

--- sorting.py
+++ sorting.py
@@ -63,7 +63,28 @@ def bubble_sort(number_array):
     return number_array
 
 
-# def insertion_sort
+def insertion_sort(number_array):
+    """
+
+    :param str number_array: list with numeric array
+    :return: sorted numeric array
+    """
+
+    # n = len(number_array)
+    # for i in range(n):
+    #     nova_oblast = number_array[i:-1]
+    #     for number in number_array:
+    n = len(number_array)
+    for i in range(1, n):
+        key = number_array[i]
+        j = i - 1
+        while j >= 0 and number_array[j] > key:
+            number_array[j + 1] = number_array[j]
+            j = 1
+        number_array[j+1] = key
+
+    return number_array
+
 
 
 def main():
@@ -71,6 +92,7 @@ def main():
     print(data)
     print(selection_sort(data["series_1"]))
     print(bubble_sort(data["series_2"]))
+    print(insertion_sort(data["series_2"]))
     pass
 
 
