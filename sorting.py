d5a9ceaf9620ecb6f467b900290bedbac39fd4c9
Tomas Prchlik <tomasp1900@gmail.com>
2025-04-14 13:15:41 +0200

    message

--- sorting.py
+++ sorting.py
@@ -24,11 +24,41 @@ def read_data(file_name):
     return data
 
 
+def selection_sort(number_array, direction = "ascending"):
+    """
+
+    :param list number_array: list with numeric array
+    :param direction: string indicating sorting direction: ascending / descending
+    :return: sorted numeric array
+    """
+    n = len(number_array)
+    for i in range(n):
+        min_max_idx = i
+        for num_idx in range(i + 1, n):
+            if direction == "ascending":
+                if number_array[num_idx] < number_array[min_max_idx]:
+                    min_max_idx = num_idx
+            elif direction == "descending":
+                if number_array[num_idx] > number_array[min_max_idx]:
+                    min_max_idx = num_idx
+
+        number_array[i], number_array[min_max_idx] = number_array[min_max_idx], number_array[i]
+
+    return number_array
+
+
+def bubble_sort(number_array):
+    """
+
+    :param str number_array: list with numeric array
+    :return: sorted numeric array
+    """
 
 
 def main():
     data = read_data("numbers.csv")
     print(data)
+    print(selection_sort(data["series_1"]))
     pass
 
 
